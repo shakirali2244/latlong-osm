a45d6eab13e1bb6401cb1ba8806c10bc2db6728e
Shakir Ali <shakirali2244@yahoo.com>
2015-03-30 23:34:41 -0700

    first commit

---
+++ SQL/roads.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR REPLACE FUNCTION "SA_dist"(
+    lati double precision,
+    longi double precision,
+    tol double precision)
+  RETURNS TABLE(road text,distance double precision) AS
+$$
+BEGIN
+
+RETURN QUERY SELECT name,ST_Distance(ST_SetSRID(ST_MakePoint(longi,lati),4326),way) FROM planet_osm_roads 
+	     WHERE ST_Distance(ST_SetSRID(ST_MakePoint(longi,lati),4326),way) <= tol
+	     ORDER BY ST_Distance(ST_SetSRID(ST_MakePoint(longi,lati),4326),way);
+END
+
+$$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION "SA_dist"(double precision, double precision,double precision)
+  OWNER TO gisuser;
